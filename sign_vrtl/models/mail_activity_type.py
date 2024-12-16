0c4823eb704d9f0cb8526bab5254249acd8adeb0
iammiracle <miracleaayodele@gmail.com>
2024-12-16 09:59:38 +0100

    odoo sign report integration

--- sign_vrtl/models/mail_activity_type.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class MailActivityType(models.Model):
-    _inherit = "mail.activity.type"
-
-    category = fields.Selection(selection_add=[('sign_request', 'Request Signature'),], ondelete={'sign_request': 'set default'})
